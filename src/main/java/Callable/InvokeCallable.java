85c26c5108e34f918e6e1f667bb8b2de242293a7
Artur <percikartur@gmail.com>
2021-08-19 19:21:46 +0200

    completableFuture

--- src/main/java/Callable/InvokeCallable.java
+++ src/main/java/Callable/InvokeCallable.java
@@ -3,6 +3,7 @@ package Callable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 public class InvokeCallable {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
