2d04a927b949b8a727e1731c28b5bc390f219138
Artur <percikartur@gmail.com>
2021-10-03 17:49:12 +0200

    H2 database configuration.

--- src/main/java/Callable/RaceCondition.java
+++ src/main/java/Callable/RaceCondition.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RaceCondition {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -23,20 +25,25 @@ public class RaceCondition {
 }
 
 class Counter {
-    private int count;
+    private AtomicInteger count = new AtomicInteger(0);
+    private AtomicBoolean atb = new AtomicBoolean(false);
 
 //    only one thread has access to method by synchronized
+//     public void increase() {
+//         System.out.println("First step");
+//         System.out.println("Second step");
+//
+//         synchronized (this){
+//             count++;}
+//
+//         System.out.println("Last step");
+//    }
+
      public void increase() {
-         System.out.println("First step");
-         System.out.println("Second step");
-
-         synchronized (this){
-             count++;}
-
-         System.out.println("Last step");
-    }
-
+//         count++;
+         count.getAndIncrement();
+     }
     public int getCount() {
-        return count;
+        return count.get();
     }
 }
